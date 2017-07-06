bf39868903fbd84896e0b7fb506dbf43a7dbe34c
Himat Parsana <himat@Himats-MacBook-Pro.local>
2017-07-06 18:00:33 +0530

    get todos

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express =require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
 var {Users} = require('./models/user');
@@ -33,7 +33,26 @@ app.get('/todos',(req,res)=>{
   })
 });
 
+app.get('/todos/:id',(req,res)=>{
 
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo)=>{
+    if(!todo)
+    {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(404).send();
+  })
+
+
+})
 
 app.listen(3000,()=>{
   console.log('Stared on port 3000')
